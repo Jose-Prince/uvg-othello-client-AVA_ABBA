fe52e05bfe6cfe8b2dc4f5a6a3d7f4a5120c156f
cmd-AJ <andrewlol774@gmail.com>
2025-06-04 17:38:00 -0600

    finished

--- othello_ai.py
+++ othello_ai.py
@@ -42,60 +42,115 @@ def valid_movements(board, player):
 def ai_move(board, player): 
     valid_moves = valid_movements(board, player)
     if valid_moves:
-        def make_move(board, row, col, player):
-            new_board = copy.deepcopy(board)
-            new_board[row][col] = player
-            directions = [(-1,-1), (-1,0), (-1,1),
-                          (0,-1),         (0,1),
-                          (1,-1), (1,0), (1,1)]
-            
-            for dr, dc in directions:
-                r, c = row +dr, col + dc
-                pieces_to_flip = []
-                while 0 <= r < 8 and 0 <= c < 8:
-                    if new_board[r][c] == -player:
-                        pieces_to_flip.append((r, c))
-                    elif new_board[r][c] == player:
-                        for pr, pc in pieces_to_flip:
-                            new_board[pr][pc] = player
-                        break
-                    else:
-                        break
-                    r += dr
-                    c += dc
-            return new_board
-
-        def evaluate(board, player):
-            return sum(cell == player for row in board for cell in row) - \
-                   sum(cell == player for row in board for cell in row)
-
-        def negamax(board, player, depth):
-            if depth == 0:
-                return evaluate(board, player), None
-
-            max_score = float("-inf")
-            best_move = None
-            for move in valid_moves:
-                new_board = make_move(board, move[0], move[1], player)
-                score, _ = negamax(new_board, -player, depth - 1)
-                score = -score
-                if score > max_score:
-                    max_score = score
-                    best_move = move
-
-            if best_move is None:
-                return evaluate(board, player), None
-
-            return max_score, best_move
-
-        _, move = negamax(board, player, depth=3)
-
-        if move is None:
-            if valid_moves:
-                return random.choice(valid_moves)
-            else:
-                return (0,0)
-
-        return move
-        #return random.choice(valid_moves)
+        _ , alphamove = max_value(board, player, 6, -INFINITY, INFINITY)
+        return alphamove
     return None
+
+
+
+
+##Heuristica con alpha prunning
+def max_value(board, player, depth, alpha, beta):
+    moves = valid_movements(board, player)
+    if depth == 0 or not moves:
+        return heuristic(board, player), None
+
+    best_score = -INFINITY
+    best_move = None
+    for move in moves:
+        new_board = apply_move(board, move, player)
+        score, _ = min_value(new_board, -player, depth - 1, alpha, beta)
+        if score > best_score:
+            best_score = score
+            best_move = move
+        if best_score >= beta:
+            break
+        alpha = max(alpha, best_score)
+    return best_score, best_move
+
+def min_value(board, player, depth, alpha, beta):
+    moves = valid_movements(board, player)
+    if depth == 0 or not moves:
+        return heuristic(board, -player), None  # note: use -player here
+
+    best_score = INFINITY
+    best_move = None
+    for move in moves:
+        new_board = apply_move(board, move, player)
+        score, _ = max_value(new_board, -player, depth - 1, alpha, beta)
+        if score < best_score:
+            best_score = score
+            best_move = move
+        if best_score <= alpha:
+            break
+        beta = min(beta, best_score)
+    return best_score, best_move
+
+INFINITY = float('inf')
+
+
+def apply_move(board, move, player):
+    from copy import deepcopy
+    opponent = -player
+    x, y = move
+    new_board = deepcopy(board)
+    new_board[x][y] = player
+
+    for dx, dy in DIRECTIONS:
+        i, j = x + dx, y + dy
+        path = []
+
+        while in_bounds(i, j) and new_board[i][j] == opponent:
+            path.append((i, j))
+            i += dx
+            j += dy
+
+        if in_bounds(i, j) and new_board[i][j] == player:
+            for px, py in path:
+                new_board[px][py] = player
+
+    return new_board
+
+def heuristic(board, player):
+    # Simple heuristic: difference in number of pieces
+    player_count = sum(row.count(player) for row in board)
+    opponent_count = sum(row.count(-player) for row in board)
+    return player_count - opponent_count
+
+def max_value(board, player, depth, alpha, beta):
+    moves = valid_movements(board, player)
+    if depth == 0 or not moves:
+        return heuristic(board, player), None
+
+    best_score = -INFINITY
+    best_move = None
+    for move in moves:
+        new_board = apply_move(board, move, player)
+        score, _ = min_value(new_board, -player, depth - 1, alpha, beta)
+        if score > best_score:
+            best_score = score
+            best_move = move
+        if best_score >= beta:
+            break
+        alpha = max(alpha, best_score)
+    return best_score, best_move
+
+def min_value(board, player, depth, alpha, beta):
+    moves = valid_movements(board, player)
+    if depth == 0 or not moves:
+        return heuristic(board, -player), None  # note: use -player here
+
+    best_score = INFINITY
+    best_move = None
+    for move in moves:
+        new_board = apply_move(board, move, player)
+        score, _ = max_value(new_board, -player, depth - 1, alpha, beta)
+        if score < best_score:
+            best_score = score
+            best_move = move
+        if best_score <= alpha:
+            break
+        beta = min(beta, best_score)
+    return best_score, best_move
+
+
